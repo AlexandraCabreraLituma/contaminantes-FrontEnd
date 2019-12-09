fcdf8fb8cd29f96f5d301ace2db7bfc5d2f3bfd2
AlexandraCabreraLituma <alecabreralituma@gmail.com>
2019-12-09 16:26:20 -0500

    Add configuration travis and Fixed Karma

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to TFM-Front!');
+    expect(page.getTitleText()).toEqual('Universidad del Azuay!');
   });
 });
